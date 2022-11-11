64a9c52fa291f73a84cfa3354911af6cc16f0e30
SaeDaa1029 <spiltkim99@gmail.com>
2022-11-11 17:55:05 +0900

    댓글 rest

--- app/src/main/java/com/example/app/controller/BoardController.java
+++ app/src/main/java/com/example/app/controller/BoardController.java
@@ -29,7 +29,6 @@ public class BoardController {
         if(criteria.getPage() == 0){
             criteria.createCriteria();
         }
-        /*검색 결과 개수와 페이징에 필요한 criteria를 통해 page amount 전달, keyword와 type 또한 전달*/
         pageDTO.createPageDTO(criteria, boardService.getTotal(criteria));
         model.addAttribute("boards", boardService.show(criteria));
         model.addAttribute("pagination", pageDTO);
